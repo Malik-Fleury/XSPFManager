c6bafc4606be5204e202e729171f5601ce5fb3f6
malik.fleury <malik.fleury@he-arc.ch>
2018-11-20 12:44:38 +0100

    Added example of model for the playlist

--- include/data/playlistmodel.h
+++ include/data/playlistmodel.h
@@ -1,7 +1,11 @@
 #ifndef PLAYLISTMODEL_H
 #define PLAYLISTMODEL_H
 
+#include <QtWidgets>
 #include <QAbstractTableModel>
+
+const int COLS= 3;
+const int ROWS= 10;
 
 class PlaylistModel : public QAbstractTableModel
 {
@@ -12,8 +16,18 @@ public:
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 
-private:
+    // Headers
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
 
+    // Editing
+    bool setData(const QModelIndex & index, const QVariant & value, int role = Qt::EditRole);
+    Qt::ItemFlags flags(const QModelIndex & index) const;
+
+public slots:
+    void timerHit();
+
+private:
+    QString m_gridData[ROWS][COLS];
 };
 
 #endif // PLAYLISTMODEL_H
