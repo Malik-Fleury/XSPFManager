3e890e94495ad0b2ddb79143991b5db49991f17e
malik.fleury <malik.fleury@he-arc.ch>
2018-12-03 16:08:53 +0100

    Added files of translations

---
+++ xspfmanager_en.ts
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>XSPF-Manager</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="30"/>
+        <source>File</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="40"/>
+        <source>Edit</source>
+        <translation>Edit</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="48"/>
+        <source>Language</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="55"/>
+        <source>Help</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="75"/>
+        <source>New</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="78"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="83"/>
+        <source>Open</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="86"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="91"/>
+        <source>Save</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="94"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="99"/>
+        <source>Save as ...</source>
+        <translation>Save as ...</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="102"/>
+        <source>Ctrl+Shift+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="107"/>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="110"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="115"/>
+        <source>Redo</source>
+        <translation>Redo</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="118"/>
+        <source>Ctrl+Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="123"/>
+        <source>Undo</source>
+        <translation>Undo</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="126"/>
+        <source>Ctrl+Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="131"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="134"/>
+        <source>Ctrl+^</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="139"/>
+        <source>Remove selected</source>
+        <translation>Removed selected</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="142"/>
+        <source>Del</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="147"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="152"/>
+        <source>French</source>
+        <translation>French</translation>
+    </message>
+</context>
+<context>
+    <name>panelExport</name>
+    <message>
+        <location filename="res/widgets/panelexport.ui" line="14"/>
+        <source>Form</source>
+        <translation>PanelExport</translation>
+    </message>
+    <message>
+        <location filename="res/widgets/panelexport.ui" line="20"/>
+        <source>Export</source>
+        <translation>Export</translation>
+    </message>
+    <message>
+        <location filename="res/widgets/panelexport.ui" line="34"/>
+        <source>Choose output</source>
+        <translation>Choose output</translation>
+    </message>
+    <message>
+        <location filename="res/widgets/panelexport.ui" line="60"/>
+        <source>Use URI</source>
+        <translation>Use URI</translation>
+    </message>
+</context>
+</TS>
---
+++ xspfmanager_fr.ts
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>XSPF-Manager</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="30"/>
+        <source>File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="40"/>
+        <source>Edit</source>
+        <translation>Edition</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="48"/>
+        <source>Language</source>
+        <translation>Langue</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="55"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="75"/>
+        <source>New</source>
+        <translation>Nouveau</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="78"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="83"/>
+        <source>Open</source>
+        <translation>Ouvrir</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="86"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="91"/>
+        <source>Save</source>
+        <translation>Sauvegarder</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="94"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="99"/>
+        <source>Save as ...</source>
+        <translation>Sauvegarder sous ...</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="102"/>
+        <source>Ctrl+Shift+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="107"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="110"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="115"/>
+        <source>Redo</source>
+        <translation>Rétablir</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="118"/>
+        <source>Ctrl+Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="123"/>
+        <source>Undo</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="126"/>
+        <source>Ctrl+Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="131"/>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="134"/>
+        <source>Ctrl+^</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="139"/>
+        <source>Remove selected</source>
+        <translation>Supprimer la sélection</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="142"/>
+        <source>Del</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="147"/>
+        <source>English</source>
+        <translation>Anglais</translation>
+    </message>
+    <message>
+        <location filename="res/window/mainwindow.ui" line="152"/>
+        <source>French</source>
+        <translation>Français</translation>
+    </message>
+</context>
+<context>
+    <name>panelExport</name>
+    <message>
+        <location filename="res/widgets/panelexport.ui" line="14"/>
+        <source>Form</source>
+        <translation>PanelExport</translation>
+    </message>
+    <message>
+        <location filename="res/widgets/panelexport.ui" line="20"/>
+        <source>Export</source>
+        <translation>Exporter</translation>
+    </message>
+    <message>
+        <location filename="res/widgets/panelexport.ui" line="34"/>
+        <source>Choose output</source>
+        <translation>Choisir une sortie</translation>
+    </message>
+    <message>
+        <location filename="res/widgets/panelexport.ui" line="60"/>
+        <source>Use URI</source>
+        <translation>Utiliser une URI</translation>
+    </message>
+</context>
+</TS>
